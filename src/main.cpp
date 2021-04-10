5a140f70d005fe06c7b598bacd839dd9fb4abaef
Lukasz <lukasz.sromek@gmail.com>
2021-04-10 16:24:44 +0200

    sorting simple listview

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-
 #include <wx/wx.h>
 #include <wx/listctrl.h>
 
@@ -19,6 +18,10 @@ public:
 private:
     wxListView *basicListView;
     void populateListView();
+    void addSingleItem(int id, const std::string &name, const std::string &description);
+    void sortById(wxCommandEvent &e);
+
+    int sortDirection = 1;
 };
 
 bool MyApp::OnInit()
@@ -33,8 +36,7 @@ wxIMPLEMENT_APP(MyApp);
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
     : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
-    wxPanel *panel = new wxPanel(this);
-    basicListView = new wxListView(panel);
+    basicListView = new wxListView(this, wxID_ANY, wxDefaultPosition, wxSize(800, 500));
     basicListView->AppendColumn("ID");
     basicListView->AppendColumn("Name");
     basicListView->AppendColumn("Description");
@@ -42,24 +44,57 @@ MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
     basicListView->SetColumnWidth(1, 120);
     basicListView->SetColumnWidth(2, 600);
 
+    auto button = new wxButton(this, wxID_ANY, "Sort by ID");
+    button->Bind(wxEVT_BUTTON, &MyFrame::sortById, this);
+
     auto sizer = new wxBoxSizer(wxVERTICAL);
     sizer->Add(basicListView, 1, wxALL | wxEXPAND, 0);
-    panel->SetSizerAndFit(sizer);
+    sizer->Add(button, 0, wxALIGN_LEFT | wxTOP | wxLEFT | wxBOTTOM, 5);
+
+    this->SetSizerAndFit(sizer);
 
     populateListView();
 }
 
 void MyFrame::populateListView()
 {
-    basicListView->InsertItem(0, "123");
-    basicListView->SetItem(0, 1, "Some Item");
-    basicListView->SetItem(0, 2, "This is an item");
+    addSingleItem(123, "Some Item", "This is an item");
+    addSingleItem(456, "Other Item", "A different item");
+    addSingleItem(102, "Another Item", "The best one!");
+}
 
-    basicListView->InsertItem(1, "456");
-    basicListView->SetItem(1, 1, "Other Item");
-    basicListView->SetItem(1, 2, "A different item");
+void MyFrame::addSingleItem(int id, const std::string &name, const std::string &description)
+{
+    int index = basicListView->GetItemCount();
 
-    basicListView->InsertItem(2, "102");
-    basicListView->SetItem(2, 1, "Another Item");
-    basicListView->SetItem(2, 2, "The best one!");
+    basicListView->InsertItem(index, std::to_string(id));
+    basicListView->SetItem(index, 1, name);
+    basicListView->SetItem(index, 2, description);
+
+    // for sorting using the SortItems method
+    basicListView->SetItemData(index, id);
+}
+
+void MyFrame::sortById(wxCommandEvent &e)
+{
+    basicListView->SortItems(
+        [](wxIntPtr item1, wxIntPtr item2, wxIntPtr direction) {
+            if (item1 == item2)
+            {
+                return 0;
+            }
+            else if (item1 < item2)
+            {
+                return -(int)direction;
+            }
+            else
+            {
+                return (int)direction;
+            }
+        },
+        this->sortDirection);
+
+    basicListView->Refresh(); // wxWidgets bug: list not reloading properly after scrolling a list with some items selected
+
+    this->sortDirection = -this->sortDirection;
 }
